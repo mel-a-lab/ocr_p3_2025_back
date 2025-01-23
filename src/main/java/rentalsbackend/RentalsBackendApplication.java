82b1d9874741a9aa756c91d5b6989e78f789ecfb
mel-a-lab <melanie.dussenne@gmail.com>
2025-01-23 14:34:22 +0100

    Ajout de SpringSecurityAuth, RentalsBackend, application.properties,pom.xml,RentalsBackendApplicationTests

---
+++ src/main/java/rentalsbackend/RentalsBackendApplication.java
@@ -0,0 +1,13 @@
+package rentalsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rentalsbackend/RentalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rentalsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
